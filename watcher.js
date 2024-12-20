55cb376620dcde2245bfe6771db9fff293b440a2
JoaquinC539 <joaquinangel@gmail.com>
2024-12-20 12:21:14 -0600

    Changed server to use a node one

--- watcher.js
+++ watcher.js
@@ -3,14 +3,14 @@ const chokidar =require("chokidar");
 const browserSync = require("browser-sync").create();
 const fs = require("fs");
 
+
+let serverProcess;
 console.log("starting file watcher...")
-
-
 
 const watcher = chokidar.watch(["./src/app"], { persistent: true });
 
 browserSync.init({
-    proxy:"http://localhost:80",
+    proxy:"http://localhost:8081",
     files:["build/**/*.*"],
     open:false,
     notify:false
@@ -34,6 +34,18 @@ function startNginx(){
     }
 }
 
+function startServer(){
+    console.log("Starting server...");;
+    try {
+        serverProcess=spawn("node",["./server/server.js"],{
+            detached :true,
+            stdio:"inherit"
+        })
+    } catch (error) {
+        console.error("Failed to start server: ",error.message);
+    }
+}
+
 
 function stopNginx() {
     console.log("Stopping Nginx server...");
@@ -43,7 +55,19 @@ function stopNginx() {
         console.error("Failed to stop Nginx:", error.message);
     }
 }
-
+function stopServer(){
+    
+    if(serverProcess && serverProcess.pid){
+        console.log(`Stopping server with PID: ${serverProcess.pid}`);  
+        try {
+            process.kill(serverProcess.pid);
+        } catch (error) {
+             console.error("Failed to stop server:", error.message);
+        }
+    }else{
+        console.log("Server process not found.");
+    }
+}
 function rebuild() {
     console.log("Change detected, rebuilding...");
     try {
@@ -55,7 +79,8 @@ function rebuild() {
 }
 
 
-startNginx();
+// startNginx();
+startServer();
 rebuild();
 watcher.on("change",(path)=>{
     console.log(`File changed: ${path}`);
@@ -71,12 +96,14 @@ console.log("File watcher is running.");
 
 process.on("SIGINT", () => {
     console.log("Process interrupted. Cleaning up...");
-    stopNginx();
+    // stopNginx();
+    stopServer();
     process.exit();
 });
 
 process.on("SIGTERM", () => {
     console.log("Process terminated. Cleaning up...");
-    stopNginx();
+    // stopNginx();
+    stopServer();
     process.exit();
 });
